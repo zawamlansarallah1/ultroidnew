db5273ee7f8e09d624554c69a5f7dc6bfb66d3ac
zawamlansarallah1 <66381319+zawamlansarallah1@users.noreply.github.com>
2021-08-17 08:36:08 +0200

    Update dm.py

--- plugins/dm.py
+++ plugins/dm.py
@@ -30,13 +30,13 @@ async def dm(e):
     masg = await e.get_reply_message()
     if e.reply_to_msg_id:
         await e.client.send_message(chat_id, masg)
-        await eod(e, "`⚜️Message Delivered!`")
+        await eod(e, "`⚜️تم إستلام الرسالة !`")
     for i in c[1:]:
         msg += i + " "
     if msg == "":
         return
     try:
         await e.client.send_message(chat_id, msg)
-        await eod(e, "`⚜️Message Delivered!⚜️`")
+        await eod(e, "`⚜️تم إستلام الرسالة  بنجاح!⚜️`")
     except BaseException:
         await eod(e, f"Read Usage : `{HNDLR}help dm`")
